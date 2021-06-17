d87cf36f3748da4c28174d6b726dc08e13981831
AntoniaChavdarova <niaaachavdarova@abv.bg>
2021-06-17 14:54:12 +0300

    Unit test coverage 94% from service functionality

--- Tests/EasyTravel.Services.Data.Tests/BookingsServiceTests.cs
+++ Tests/EasyTravel.Services.Data.Tests/BookingsServiceTests.cs
@@ -51,6 +51,133 @@
         }
 
         [Fact]
+        public async Task MakeBookingAsyncShouldThrowExceptionWhenDatesAreNotValid()
+        {
+            DateTime checkIn = new DateTime(2020, 6, 10);
+            DateTime checkOut = new DateTime(2020, 6, 5);
+
+            var service = new BookingsService(this.repositoryBooking, this.repositoryProperty);
+
+            await Assert.ThrowsAsync<Exception>(async () => await service.MakeBookingAsync("1", 1, checkIn, checkOut, 3));
+        }
+
+        [Fact]
+        public async Task MakeBookingAsyncShouldThrowExceptionWhenDatesAreNotAvailable()
+        {
+            DateTime checkIn = new DateTime(2021, 9, 18);
+            DateTime checkOut = new DateTime(2021, 9, 25);
+
+            var booking = new Booking
+            {
+                CheckIn = checkIn,
+                CheckOut = checkOut,
+                PropertyId = 1,
+            };
+            await this.repositoryBooking.AddAsync(booking);
+            await this.repositoryBooking.SaveChangesAsync();
+
+            var service = new BookingsService(this.repositoryBooking, this.repositoryProperty);
+
+            await Assert.ThrowsAsync<Exception>(async () => await service.MakeBookingAsync("1", 1, checkIn, checkOut, 3));
+        }
+
+        [Fact]
+        public async Task MakeBookingAsyncShouldThrowExceptionWhenCheckIsInBettweenExistingReservation()
+        {
+            DateTime checkIn = new DateTime(2021, 9, 19);
+            DateTime checkOut = new DateTime(2021, 9, 25);
+
+            var booking = new Booking
+            {
+                CheckIn = checkIn,
+                CheckOut = checkOut,
+                PropertyId = 1,
+            };
+            await this.repositoryBooking.AddAsync(booking);
+            await this.repositoryBooking.SaveChangesAsync();
+
+            var service = new BookingsService(this.repositoryBooking, this.repositoryProperty);
+
+            await Assert.ThrowsAsync<Exception>(async () => await service.MakeBookingAsync("1", 1, new DateTime(2021, 9, 20), new DateTime(2021, 9, 26), 3));
+        }
+
+        [Fact]
+        public async Task MakeBookingAsyncShouldThrowExceptionWhenCheckOutInBettweenExistingReservation()
+        {
+            DateTime checkIn = new DateTime(2021, 9, 19);
+            DateTime checkOut = new DateTime(2021, 9, 25);
+
+            var booking = new Booking
+            {
+                CheckIn = checkIn,
+                CheckOut = checkOut,
+                PropertyId = 1,
+            };
+            await this.repositoryBooking.AddAsync(booking);
+            await this.repositoryBooking.SaveChangesAsync();
+
+            var service = new BookingsService(this.repositoryBooking, this.repositoryProperty);
+
+            await Assert.ThrowsAsync<Exception>(async () => await service.MakeBookingAsync("1", 1, new DateTime(2021, 9, 20), new DateTime(2021, 9, 23), 3));
+        }
+
+        [Fact]
+        public async Task MakeBookingAsyncShouldThrowExceptionWhenMaxGuestCountIsBiggerThanPropertyCapacity()
+        {
+            var property = new Property
+            {
+                Capacity = 3,
+                Id = 1,
+            };
+            await this.repositoryProperty.AddAsync(property);
+            await this.repositoryProperty.SaveChangesAsync();
+
+            var service = new BookingsService(this.repositoryBooking, this.repositoryProperty);
+
+            await Assert.ThrowsAsync<Exception>(async () => await service.MakeBookingAsync("1", 1, new DateTime(2021, 9, 20), new DateTime(2021, 9, 23), 4));
+        }
+
+        [Fact]
+        public async Task MakeBookingAsyncShouldThrowExceptionWhenDatesBetweenWxistingResevation()
+        {
+            DateTime checkIn = new DateTime(2021, 9, 18);
+            DateTime checkOut = new DateTime(2021, 9, 25);
+
+            var booking = new Booking
+            {
+                CheckIn = checkIn,
+                CheckOut = checkOut,
+                PropertyId = 1,
+            };
+            await this.repositoryBooking.AddAsync(booking);
+            await this.repositoryBooking.SaveChangesAsync();
+
+            var service = new BookingsService(this.repositoryBooking, this.repositoryProperty);
+
+            await Assert.ThrowsAsync<Exception>(async () => await service.MakeBookingAsync("1", 1, new DateTime(2021, 9, 19), new DateTime(2021, 9, 21), 3));
+        }
+
+        [Fact]
+        public async Task MakeBookingAsyncShouldWorkCorrectly()
+        {
+            DateTime checkIn = new DateTime(2021, 9, 18);
+            DateTime checkOut = new DateTime(2021, 9, 25);
+            var property = new Property
+            {
+                Capacity = 3,
+                Id = 1,
+            };
+            await this.repositoryProperty.AddAsync(property);
+            await this.repositoryProperty.SaveChangesAsync();
+
+            var service = new BookingsService(this.repositoryBooking, this.repositoryProperty);
+
+            await service.MakeBookingAsync("1", 1, checkIn, checkOut, 2);
+
+            Assert.Equal(1, this.repositoryBooking.All().Count());
+        }
+
+        [Fact]
         public async Task ShoulDeleteBookingForUserId()
         {
             var booking1 = new Booking
@@ -101,19 +228,7 @@
         {
             public int Id { get; set; }
 
-            //public int PeopleCount { get; set; }
-
-            //public DateTime CheckIn { get; set; }
-
-            //public DateTime CheckOut { get; set; }
-
             public string UserId { get; set; }
-
-            //public int PropertyId { get; set; }
-
-            //public string PropertyName { get; set; }
-
-            //public bool? Confirmed { get; set; }
         }
     }
 }
